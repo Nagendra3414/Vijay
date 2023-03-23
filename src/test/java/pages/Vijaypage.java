1e7d932aebde0f1cdbd33a0eeeb19ee8463714cf
Nagendra3414 <gadinagendra3414@gmail.com>
2023-03-23 11:21:53 +0530

    Vijay commited project with adding one class

---
+++ src/test/java/pages/Vijaypage.java
@@ -0,0 +1,12 @@
+package pages;
+
+public class Vijaypage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Added new class in my script");
+	}
+
+}
